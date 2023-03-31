56e7a134439e6ae40dfff27158c0e3d8a010fddf
Rique2 <enrique2heinrich@gmail.com>
2023-03-31 13:27:11 +0200

    Add game.test

--- src/game.js
+++ src/game.js
@@ -1,1 +1,9 @@
-console.log("webpack im here");
+import Player from './modules/player'
+
+const setPlayers = function () {
+  const playerOne = Player('user')
+  const playerTwo = Player('computer')
+  return [playerOne, playerTwo]
+}
+
+export { setPlayers }
